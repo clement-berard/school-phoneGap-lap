4cb2f74f59e5c2a9992826d0f16492ead9833c3f
Clement BERARD <keload@gmail.com>
2014-12-04 16:27:05 +0100

    up initial

---
+++ js/main-config.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// objet javascript contenant un ensemble de function et de constante pour notre application
+var mainConfig = {
+    myParam: 'some string var',
+    myFunction: function () {
+        // some code here
+    }
+};
+
+
---
+++ js/urlHelper.js
@@ -0,0 +1,79 @@
+/**
+ *
+ */
+$('.goPage').on('click', function () {
+    goPage($(this).attr('href'), $(this).data('params'));
+    return false;
+});
+
+/**
+ *
+ * @param url
+ * @param param
+ */
+function goPage(url, param) {
+    var p = (param != null) ? url + "?parameters=" + param : url;
+    if (param != null) {
+        $.mobile.changePage(url, {
+            dataUrl: p,
+            data: {'parameters': param},
+            reloadPage: true,
+            changeHash: true
+        });
+
+    }
+    else {
+        $.mobile.changePage(url, {
+            dataUrl: p,
+            reloadPage: true,
+            changeHash: true
+        });
+    }
+}
+
+/**
+ *
+ * @param t
+ * @returns {XML|string|void}
+ */
+function getObjParameters(t) {
+    if(t.data("url").split("?")[1] != undefined){
+        var parameters = (t.data("url").split("?")[1]).replace("parameters=", "");
+        var array = parameters.split(",");
+        var result = "";
+        $.each(array, function (i) {
+            result += quote(array[i]) + ',';
+        });
+        result = result.substring(0, result.length - 1);
+        result = '{' + result + '}';
+        parameters = $.parseJSON(result);
+        return parameters;
+    }
+    else{
+        return $.parseJSON('{}');
+    }
+}
+
+/**
+ *
+ * @param text
+ * @returns {string}
+ */
+function quote(text) {
+    var urls = text.split(':');
+    for (var i = 0; i < urls.length; i++) urls[i] = '"' + urls[i] + '"';
+    return urls.join(":")
+}
+
+/**
+ *
+ * @returns {boolean}
+ */
+function goBack() {
+    parent.history.back();
+    return false;
+}
+
+
+
+
