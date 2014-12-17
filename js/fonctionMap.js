03d499ceb5687792db10fba2f8d0115a9e811271
Clement BERARD <keload@gmail.com>
2014-12-17 15:34:37 +0100

    commentaires pour fonctionMap.js

--- js/fonctionMap.js
+++ js/fonctionMap.js
@@ -1,36 +1,61 @@
+/**
+ * Ce fichier contient les ensembles de fonctions de creation d'une map google
+ * @param myMapId
+ */
+
 function fonctionMap(myMapId) {
 
 
+    // id de la div contenant la map
+    this.mymapid = myMapId,
+        // objet map Google instancié lors de l'init
+        this.Mymap = null,
+        // object contenant l'ensemble de markers
+        this.markers = {},
 
-    this.mymapid  =  myMapId,
-    this.Mymap = null,
-    this.markers = {},
 
+    /**
+     * fonction d'initialisation de la map (on appelle juste une autre fonction ici)
+     */
+        this.init = function () {
+            this.createMap();
+            // on retourne notre objet JS pour s'en servir dans notre code
+            return this;
+        },
 
+    /**
+     * getter pour la map Google
+     */
+        this.getMap = function () {
+            return this.Mymap;
+        },
 
-    this.init = function() {
-        this.createMap();
-        return this;
-    },
-   
-
-    this.getMap = function(){
-        return this.Mymap;
-    },
-
-    this.getMarker = function (key) {
+    /**
+     * pour recuperer un marker
+     */
+        this.getMarker = function (key) {
             return this.markers[key];
         },
 
-    this.getMarkers = function () {
+    /**
+     * pour recuperer l'ensemble des markers de notre map
+     */
+        this.getMarkers = function () {
             return this.markers;
         },
 
-    this.getSize = function(){
-        return this.markers.length;
-    }    
+    /**
+     * retourne le nombre de markers de la map
+     */
+        this.getSize = function () {
+            return this.markers.length;
+        }
 
-    this.createMap = function() {
+    /**
+     * fonction de creation de la map google
+     */
+    this.createMap = function () {
+        // initialisation arbitraire
         init_latlng = this.getGoogleLocation('45.885379', '4.247927');
         var myOptions = {
             zoom: 9,
@@ -39,49 +64,55 @@ function fonctionMap(myMapId) {
         };
         geocoder = geocoder = new google.maps.Geocoder();
         map = new google.maps.Map(document.getElementById(this.mymapid), myOptions);
-        this.Mymap =  map;
-
+        this.Mymap = map;
     },
-
-    this.uniqid = function () {
+    /**
+     * fonction qui retourne un nombre unique. sert pour l'enregistrement de le localstorage
+     */
+        this.uniqid = function () {
             var n = Math.floor(Math.random() * 11);
             var k = Math.floor(Math.random() * 1000000);
             return k;
         },
-
-    this.moveToLocation = function(map,lat, lng) {
+    /**
+     * pour modifier le centre de la map avec de nouvelles coordonnees
+     */
+        this.moveToLocation = function (map, lat, lng) {
             var center = new google.maps.LatLng(lat, lng);
             // using global variable:
             map.panTo(center);
-    },
+        },
+    /**
+     * retourne un objet LatLng de google en fonction de coordonnees
+     */
+        this.getGoogleLocation = function (lat, lng) {
+            return new google.maps.LatLng(lat, lng);
+        },
+    /**
+     * pour ajouter un marker a la map, et le mettre avec l'ensemble des markers de cette map
+     */
+        this.addMarker = function (name, loc, content) {
 
-
-    this.getGoogleLocation =  function(lat,lng){
-        return  new google.maps.LatLng(lat, lng);
-    },
-
-    this.addMarker = function(name, loc, content) {
-
-        var m = new google.maps.Marker({
-            position: loc,
-            map: this.Mymap
-        });
-        this.markers.name = m;
-
-        if (content != null) {
-            var c = new google.maps.InfoWindow({
-                content: content
+            var m = new google.maps.Marker({
+                position: loc,
+                map: this.Mymap
             });
-            c.open(this.Mymap, m);
+            this.markers.name = m;
+            // si on veut ajouter une infobulle sur le marker, et on l'ouvre directement
+            if (content != null) {
+                var c = new google.maps.InfoWindow({
+                    content: content
+                });
+                c.open(this.Mymap, m);
+            }
+        },
+    /**
+     * pour supprimer un marker de la map
+     */
+        this.deleteMarker = function (tab, name) {
+            if (markers.name != null)
+                markers.name.setMap(null);
         }
-
-
-    },
-
-    this.deleteMarker = function(tab,name){
-        if (markers.name != null)
-            markers.name.setMap(null);
-    }
 };
 
 
