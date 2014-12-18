f1036872100dbed60bb8131c5441f39a92df86a8
Clement BERARD <keload@gmail.com>
2014-12-18 15:41:15 +0100

    rm du urHelper

--- js/main-config.js
+++ js/main-config.js
@@ -1,6 +1,7 @@
 "use strict";
 
 // objet javascript contenant un ensemble de function et de constante pour notre application
+// a la date du rendu du projet, on ne s'en sert pas, mais on pourrait en avoir besoin plus tard !
 var mainConfig = {
     myParam: 'some string var',
     myFunction: function () {
